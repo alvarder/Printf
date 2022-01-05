0312437a964d6f4e32536684ba1693a125d1d565
Alvaro <alvaricoque78@gmail.com>
2022-01-05 18:58:42 +0100

    2

--- Source/ft_putchar_fd.c
+++ Source/ft_putchar_fd.c
@@ -1,24 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ajordan- <ajordan-@student.42urduliz.com>  +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/02 12:40:20 by ajordan-          #+#    #+#             */
-/*   Updated: 2021/10/19 14:24:04 by ajordan-         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-/*
-*	DESCRIPTION
-*	Outputs the character ’c’ to the given file descriptor.
-*	PARAMETERS
-*	#1. The character to output.
-*	#2. The file descriptor on which to write.
-*	RETURN VALUES
-*	-
-*/
 
 #include "libft.h"
 
